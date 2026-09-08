fcc67f9207b2fdeeb85003c60ea569927a70f16d
Salman Butt <47062638+salman0butt@users.noreply.github.com>
2026-09-09 01:04:15 +0500

    test: align knowledge detail route count

--- tests/Unit/Admin/KnowledgeDetailRoutesTest.php
+++ tests/Unit/Admin/KnowledgeDetailRoutesTest.php
@@ -52,7 +52,7 @@ final class KnowledgeDetailRoutesTest extends TestCase {
 					)
 				);
 		}
-		Functions\expect( 'register_rest_route' )->times( 7 )->withAnyArgs();
+		Functions\expect( 'register_rest_route' )->times( 9 )->withAnyArgs();
 
 		AdminRestBootstrap::register_routes();
 	}
