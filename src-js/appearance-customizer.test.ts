663eb907a2ce9e9ec4326cf5b681472e4ca5660e
Salman Butt <47062638+salman0butt@users.noreply.github.com>
2026-09-13 08:55:12 +0500

    test: finish Task 8B RED formatting

--- src-js/appearance-customizer.test.ts
+++ src-js/appearance-customizer.test.ts
@@ -52,9 +52,7 @@ const createTestElement = (
 
 	for ( const child of children ) {
 		if ( child !== undefined ) {
-			element.append(
-				child instanceof Node ? child : String( child )
-			);
+			element.append( child instanceof Node ? child : String( child ) );
 		}
 	}
 
